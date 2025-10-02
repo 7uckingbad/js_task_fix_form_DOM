888d67c4b700cc58cc75377c4fa679429d8a418b
Denis M <denismushta@gmail.com>
2025-10-03 00:41:53 +0300

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,9 +10,19 @@ forms.forEach((form) => {
     const label = document.createElement('label');
 
     label.classList.add('field-label');
+
+    if (!input.id) {
+      input.id = `input-${Math.random().toString(36).substring(2, 9)}`;
+    }
+
     label.setAttribute('for', input.id);
     label.textContent = input.name;
     input.parentElement.appendChild(label);
     input.placeholder = input.name.toUpperCase();
+
+    if (input.name) {
+      input.placeholder =
+        input.name.charAt(0).toUpperCase() + input.name.slice(1);
+    }
   });
 });
